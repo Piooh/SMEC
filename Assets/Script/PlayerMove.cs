322dd433acb8e3eaa385c4e96d4affbde121d245
Piooh <piohsmec@gmail.com>
2016-03-12 17:17:37 +0900

    2016/03/12

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour 
+{
+	private void Awake()
+	{
+		transform.position = Vector3.right * 5;
+		//		transform.localPosition = Vector3.right * 5;
+	}
+}
---
+++ Assets/Script/PlayerRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour 
+{
+
+	private void Update()
+	{
+		if (true == Input.GetKeyUp (KeyCode.Space)) 
+		{
+			transform.rotation = Quaternion.identity;
+			Debug.Log ("Reset World");
+		} 
+		else if (true == Input.GetKeyUp (KeyCode.Escape)) 
+		{
+			transform.localRotation = Quaternion.identity;
+			Debug.Log ("Reset Local");
+		}
+	}
+}
