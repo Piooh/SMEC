2d014404b3f0aa3233246cb8fd7ceabd2c998026
Pioh <piohsmec@gmail.com>
2016-03-23 21:51:30 +0900

    애니메이션 적용중.

---
+++ Assets/Script/Lecture 4/AnimationControll.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum AnimationState : int
+{
+	Idle	= 0,
+	Move	= 1,
+	Lose	= 99,
+	Win		= 100,
+}
+
+public class AnimationControll : MonoBehaviour 
+{
+	private Animator animator		= null;
+	private PlayerMove playerMove	= null;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+		playerMove = GetComponent<PlayerMove>();
+
+		MyDebug.Assert( animator, "Can not find Animator Component" );
+		MyDebug.Assert( playerMove, "Can not find PlayerMove Component" );
+
+		playerMove.onMove += OnMove;
+	}
+
+	private void OnMove( float moveScale )
+	{
+		animator.SetBool( "Move", 0f != moveScale );
+	}
+
+}
--- Assets/Script/Lecture 4/PlayerMove.cs
+++ Assets/Script/Lecture 4/PlayerMove.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class PlayerMove : MonoBehaviour
 {
 	private float speed = 5f;
+
+	public System.Action<float> onMove = null;
 	
 	private void FixedUpdate()
 	{
 		Move();
 	}
 
-
 	private void Move()
 	{
-		var vertical	= Input.GetAxis( "Vertical" );
-		var horizontal	= Input.GetAxis( "Horizontal" );
+		var vertical		= Input.GetAxis( "Vertical" );
+		var horizontal		= Input.GetAxis( "Horizontal" );
 
 		var direction		= (Vector3.forward * vertical) + (Vector3.right * horizontal);
 		direction.Normalize();
@@ -24,6 +26,13 @@ public class PlayerMove : MonoBehaviour
 			transform.rotation = Quaternion.LookRotation( direction, Vector3.up );
 		}
 
-		transform.position	= transform.position + (direction * speed * Time.fixedDeltaTime);
+		var moveScale		= (direction * speed * Time.fixedDeltaTime);
+
+		transform.position	= transform.position + moveScale;
+
+		if( null != onMove )
+		{
+			onMove( moveScale.sqrMagnitude );
+		}
 	}
 }
