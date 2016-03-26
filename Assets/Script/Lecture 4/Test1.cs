0c08a53c7d9745abfe26d4c647651deca0688805
piooh <piohsmec@gmail.com>
2016-03-26 14:28:49 +0900

    pull request #11 success Test

--- Assets/Script/Lecture 4/Test1.cs
+++ Assets/Script/Lecture 4/Test1.cs
@@ -1,15 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Test1 : MonoBehaviour {
+public class Test1 : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
