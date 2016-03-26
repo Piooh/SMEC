84c381640c67e676f85620c547d4e20699addf5f
piooh <piohsmec@gmail.com>
2016-03-26 13:37:19 +0900

    error code

---
+++ Assets/Script/Lecture 3/Error.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Error : MonoBehaviour 
+{
+	public GameObject target = null;
+
+	private void Awake()
+	{
+		//컴파일 오류
+//		int a = 1
+
+		// 런타임 오류
+//		Debug.Log (target.name);
+
+
+		// 논리 오류.
+		int from = 0;
+		int to = 100000000;
+
+		var doubleFrom = System.Convert.ToDouble (from);
+		var doubleTo = System.Convert.ToDouble (to);
+
+
+		for (double factor = 0.1f; factor <= 1.0; factor += 0.1 ) 
+		{
+			int interpolation = (int)System.Convert.ToDouble( (from * (1.0f - factor) + to * factor) );
+
+			Debug.LogError (interpolation);
+
+		}
+	}
+}
