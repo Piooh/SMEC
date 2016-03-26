c2c9623b8ce0d25aa7b1cb276943a0c2fc2de2d3
piooh <piohsmec@gmail.com>
2016-03-26 14:13:24 +0900

    fiexed #10 캐릭터 클래스 제작 완료.

--- Assets/Script/Lecture 4/Character.cs
+++ Assets/Script/Lecture 4/Character.cs
@@ -1,15 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
